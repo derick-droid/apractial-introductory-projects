c50a881d41a703bcc03aef0f10874c3e47fb9099
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-26 20:13:53 +0300

    Delete fibonacci.py

--- fibonacci.py
+++
@@ -1,4 +0,0 @@
-# 9. The Fibonacci numbers are the sequence below, where the first two numbers are 1, and each
-# number thereafter is the sum of the two preceding numbers. Write a program that asks the
-# user how many Fibonacci numbers to print and then prints that many.
-
