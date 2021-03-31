c8212becc6c9cd7ef06278ae2e5dce7f2b771231
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-03-31 12:10:14 +0300

    Add files via upload
    question 2 of a practical introduction to python topic of numbers

---
+++ randomxy.py
@@ -0,0 +1,11 @@
+
+# Write a program that generates a random number, x , between 1 and 50, a random number y
+# between 2 and 5, and computes x y .
+
+from random import randint
+x = randint(1, 50)
+y = randint(2, 5)
+print("the random of x = ", x)
+print("the random of y = ", y)
+
+print(x**y)
