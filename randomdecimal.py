37905c4e6d33f813a89bd94c66789a9b7694039f
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-03-31 12:36:32 +0300

    Add files via upload
    quiz 4

---
+++ randomdecimal.py
@@ -0,0 +1,8 @@
+# Write a program that generates a random decimal number between 1 and 10 with two decimal
+# places of accuracy. Examples are 1.23, 3.45, 9.80, and 5.00.
+#
+from random import randint
+number = randint(1, 10)
+number1 = float(number)
+print(number1)
+
