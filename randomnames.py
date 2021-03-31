1b66d10cabec3d85e3470432b301a88eb2ae2df1
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-03-31 12:22:11 +0300

    Add files via upload
    question 3 of a practical introduction of python

---
+++ randomnames.py
@@ -0,0 +1,6 @@
+# Write a program that generates a random number between 1 and 10 and prints your name
+# that many times.
+from random import randint
+x = randint(1, 10)
+
+print("derrick okinda " * x )
