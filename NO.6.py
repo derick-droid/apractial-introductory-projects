e26fb91b93300a1806ecc6b8fcb088f266b76028
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-04-04 09:07:32 +0300

    Add files via upload
    no.6 in numbers

---
+++ NO.6.py
@@ -0,0 +1,11 @@
+# Write a program that asks the user to enter two numbers, x and y , and computes
+# |x− y|
+# x+ y .
+
+num1 = int(input("enter NO: "))
+num2 = int(input("enter NO: "))
+
+sub = abs(num1 - num2)
+add = num1 + num2
+print(sub)
+print(add)
