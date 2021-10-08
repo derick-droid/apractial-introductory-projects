8ee7072d3f715ed769cb75da04438fad7939d50c
derick-droid <derrickokinda9@gmail.com>
2021-10-08 14:01:47 +0300

    python practicals in string

---
+++ strings/basics/basic_string.py
@@ -0,0 +1,23 @@
+"""
+creating strings
+"""
+s = "hello programmer"
+t = "hello attaches"
+long = """
+this is the longest string in the program it should contain three quotation marks 
+to make it more effective
+"""
+
+print(s)
+print(t)
+print(long)
+
+# inputs
+number = eval(input("enter a number: "))
+string = input("enter your name: ")
+
+# empty string
+print("")
+
+# length
+print(len(long))
