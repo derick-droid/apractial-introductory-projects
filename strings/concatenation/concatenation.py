eba3088e8f284c8ce051a99deeb2a5262c23d67e
derick-droid <derrickokinda9@gmail.com>
2021-10-08 15:02:25 +0300

    python practicals practices

---
+++ strings/concatenation/concatenation.py
@@ -0,0 +1,15 @@
+# concatenation
+print("ab" + "cd")
+
+# repetition
+print("hi " * 70)
+
+# example
+s = " "
+print(s)
+for i in range(10):
+    letter = input("enter a letter: ")
+    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+        s = s + letter
+
+print(s)
