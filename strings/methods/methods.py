a090529bcc71618e2debc66a155e7acdfd73ae88
derick-droid <derrickokinda9@gmail.com>
2021-10-10 11:11:00 +0300

    python simple practicals1

--- strings/methods/methods.py
+++ strings/methods/methods.py
@@ -2,4 +2,10 @@
 methods associated with strings
 
 """
+# isalpha() checks if a string is only a letter
 
+letter = input("enter string: ")
+if letter[0].isalpha():
+    print("it has no number")
+else:
+    print("it has number")
