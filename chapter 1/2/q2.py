f1110ce927dff0a27557af51abccd5d1af7a4ee0
derick-droid <derrickokinda9@gmail.com>
2022-08-24 18:03:54 +0300

    recap 3

--- chapter 1/2/q2.py
+++ chapter 1/2/q2.py
@@ -3,7 +3,7 @@ def pattern2():
    print("*" * 20)
    print("*" * 20)
    print("*" * 20)
-   
+
    return True
 
 pattern2()
---
+++ for/main.py
@@ -0,0 +1,20 @@
+# def forx():
+#     for name in range(10):
+#         print("derrick")
+#
+#     return True
+#
+# forx()
+
+def for1():
+    for i in range(3):
+        number = int(input ("enter number: "))
+        ans = number * number
+        print(ans)
+    print("the loop is done")
+    return True
+
+
+
+for1()
+
