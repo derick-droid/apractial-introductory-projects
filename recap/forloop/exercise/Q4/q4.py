07386053972f34dde91455aed63b6355cf21d21a
derick-droid <derrickokinda9@gmail.com>
2022-08-25 10:42:41 +0300

    question5

---
+++ recap/forloop/exercise/Q4/q4.py
@@ -0,0 +1,12 @@
+"""
+4. Write a program that prints out a list of the integers from 1 to 20 and their squares. The output
+"""
+
+
+def lists():
+    for i in range(1, 20):
+        print(i + 1, (i+1) * (i + 1))
+    return True
+
+
+lists()
