bd1402989564f51371ad25c2a3b0af7e9fc52ac3
derick-droid <derrickokinda9@gmail.com>
2022-08-30 15:38:53 +0300

    question 7

---
+++ recap/forloop/exercise/Q7/q7.py
@@ -0,0 +1,24 @@
+"""
+Write a program that uses exactly four for loops to print the sequence of letters below.
+"""
+
+
+def numbers():
+    for i in range(10):
+        print("A", end=" ")
+    for j in range(7):
+        print("B", end=" ")
+    for m in range(5):
+        print("C", end=" ")
+        print("D", end=" ")
+    print("E", end=" ")
+
+    for z in range(6):
+        print("F", end=" ")
+    print("G")
+
+    return True
+
+
+numbers()
+
