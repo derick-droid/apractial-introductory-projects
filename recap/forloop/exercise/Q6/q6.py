4eae8a9e45755363d36babfe1254898a1c0ee7e2
derick-droid <derrickokinda9@gmail.com>
2022-08-30 15:31:34 +0300

    question 6

---
+++ recap/forloop/exercise/Q6/q6.py
@@ -0,0 +1,13 @@
+"""
+Write a program that uses a for loop to print the numbers 100, 98, 96, . . . , 4, 2.
+"""
+
+
+def number():
+    for x in range(100, 0, -2):
+        print(x)
+
+    return True
+
+
+number()
