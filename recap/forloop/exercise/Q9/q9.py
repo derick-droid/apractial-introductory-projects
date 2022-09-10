7491dd5e74e7caf112a6492165793eb7329c0f28
derick-droid <derrickokinda9@gmail.com>
2022-09-10 09:29:31 +0300

    more recap

---
+++ recap/forloop/exercise/Q9/q9.py
@@ -0,0 +1,15 @@
+# """
+# The Fibonacci numbers are the sequence below, where the first two numbers are 1, and each
+# number thereafter is the sum of the two preceding numbers. Write a program that asks the
+# user how many Fibonacci numbers to print and then prints that many.
+#
+# """
+#
+#
+# def fibanocci():
+#     first = 1
+#     for i in range(1,100):
+#         print(first, first+i, i+i ,end=" ")
+#     return  True
+#
+# fibanocci()
