deb8d3cccdd36e5f2eb3e66fdae139628f26edb5
derick-droid <derrickokinda9@gmail.com>
2022-09-11 20:55:11 +0300

    quiz 10 patterns

---
+++ recap/forloop/exercise/Q10/main.py
@@ -0,0 +1,14 @@
+"""
+Use a for loop to print a box like the one below. Allow the user to specify how wide and how
+high the box should be. [Hint: print('*'*10) prints ten asterisks.]
+
+"""
+
+
+def patterns():
+    for i in range(4):
+        print("*" * 10)
+
+    return True
+
+patterns()
