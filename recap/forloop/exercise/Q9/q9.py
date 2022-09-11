66ab0799868eb8d1cbf084fd4625f7b8e5219a15
derick-droid <derrickokinda9@gmail.com>
2022-09-11 20:52:29 +0300

    question 9 fibonaci series

--- recap/forloop/exercise/Q9/q9.py
+++ recap/forloop/exercise/Q9/q9.py
@@ -1,15 +1,23 @@
-# """
-# The Fibonacci numbers are the sequence below, where the first two numbers are 1, and each
-# number thereafter is the sum of the two preceding numbers. Write a program that asks the
-# user how many Fibonacci numbers to print and then prints that many.
-#
-# """
-#
-#
-# def fibanocci():
-#     first = 1
-#     for i in range(1,100):
-#         print(first, first+i, i+i ,end=" ")
-#     return  True
-#
-# fibanocci()
+"""
+The Fibonacci numbers are the sequence below, where the first two numbers are 1, and each
+number thereafter is the sum of the two preceding numbers. Write a program that asks the
+user how many Fibonacci numbers to print and then prints that many.
+
+"""
+
+
+def fib():
+    times = int(input("enter number of times: "))
+
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(2, times):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+
+
+fib()
